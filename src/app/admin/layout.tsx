b09561771e77f2820787f9f301464b05c5c8bacf
Sarvesh <andrewstyles467@gmail.com>
2026-04-08 19:51:14 +0800

    fix: dynamic server rendering for layouts

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,7 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header/AdminHeader";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
